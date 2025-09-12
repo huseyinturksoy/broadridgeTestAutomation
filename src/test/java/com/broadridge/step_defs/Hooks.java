c35c2b9066ec8f256d1a01b3e38a5c6ca8e6bf1b
huseyin-turksoy <huseyin.turksoy@nmqdigital.com>
2025-09-13 01:46:49 +0300

    both form is working

--- src/test/java/com/broadridge/step_defs/Hooks.java
+++ src/test/java/com/broadridge/step_defs/Hooks.java
@@ -1,22 +1,26 @@
 package com.broadridge.step_defs;
 
 import com.broadridge.utils.Driver;
+import io.cucumber.java.BeforeStep;
 import io.cucumber.java.Scenario;
-import org.junit.After;
+import io.cucumber.java.After;
 import org.junit.AfterClass;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
+
+    @BeforeStep
+    public void horizontalLineStart(){
+        System.err.println("------------------- START STEP -------------------");
+
+    }
+
     @After
-    public void tearDown(Scenario scenario){
+    public void tearDown(){
 
         //if scenario fails, it takes screenshot
-
-        if (scenario.isFailed()){
-            byte[] screenshot = ( (TakesScreenshot) Driver.getDriver() ).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", scenario.getName());
-        }
+        System.err.println("------------------- END BROWSER -------------------");
 
         //for closing the browser
         Driver.closeDriver();
