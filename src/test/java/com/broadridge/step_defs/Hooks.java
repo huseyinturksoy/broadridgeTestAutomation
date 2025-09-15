234b7d0c3e4627a9b47f1ce0cb214c409a6891bb
huseyin-turksoy <huseyin.turksoy@nmqdigital.com>
2025-09-15 10:27:58 +0300

    mvn is updated

--- src/test/java/com/broadridge/step_defs/Hooks.java
+++ src/test/java/com/broadridge/step_defs/Hooks.java
@@ -17,7 +17,12 @@ public class Hooks {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
+
+        if (scenario.isFailed()){
+            byte[] screenshot = ( (TakesScreenshot) Driver.getDriver() ).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
 
         //if scenario fails, it takes screenshot
         System.err.println("------------------- END BROWSER -------------------");
